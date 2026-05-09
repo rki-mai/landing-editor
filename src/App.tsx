6e03207808438146e03c6275c85149ffc6d8b8c2
Nifacy <mai.grishin.alexey@gmail.com>
2026-05-09 23:14:00 +0300

    remove json editor component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { renderElements } from "./components/editor_components_renderer";
-import { JsonEditor } from "./components/json_editor";
-import { validateLandingPage } from "./components/parser";
 import { PreviewCanvas } from "./components/preview_canvas";
 import "./App.css";
 import {
@@ -25,7 +23,7 @@ import { moveElementHandler } from "./components/handlers/move_element";
 import { landingElementUpdater } from "./components/handlers/update_element";
 import { type LandingElement, type LandingPage } from "./components/types";
 
-const DEFAULT_DATA = JSON.stringify({ elements: [] }, null, 2);
+const DEFAULT_PAGE: LandingPage = { elements: [] };
 
 function findElementById(page: LandingPage, elementId: string): LandingElement {
 	for (const element of page.elements) {
@@ -38,46 +36,23 @@ function findElementById(page: LandingPage, elementId: string): LandingElement {
 }
 
 function App() {
-	const [landingData, setLandingData] = useState<string>(DEFAULT_DATA);
-	const [landingPage, setLandingPage] = useState<LandingPage | null>(null);
-	const [errorMessage, setErrorMessage] = useState<string | null>(null);
+	const [landingPage, setLandingPage] = useState<LandingPage>(DEFAULT_PAGE);
 	const [settingsElementId, setSettingsElementId] = useState<string | null>(
 		null,
 	);
 
-	const updateData = (updated: LandingPage) => {
-		setLandingPage(updated);
-		setLandingData(JSON.stringify(updated, null, 2));
-	};
 	const onSettingsOpened = (element: LandingElement) => {
 		setSettingsElementId(element.id);
 	};
 
-	const updater = landingElementUpdater(landingPage, updateData);
-	const moveHandler = moveElementHandler(landingPage, updateData);
-	const createElement = createElementHandler(landingPage, updateData);
-	const deleteHandler = deleteElementHandler(landingPage, updateData);
-
-	const handleRender = (data: string) => {
-		try {
-			setLandingPage(validateLandingPage(JSON.parse(data)));
-			setErrorMessage(null);
-		} catch (err: unknown) {
-			if (err instanceof Error) {
-				setErrorMessage("Invalid JSON data: " + err.toString());
-			}
-		}
-	};
+	const updater = landingElementUpdater(landingPage, setLandingPage);
+	const moveHandler = moveElementHandler(landingPage, setLandingPage);
+	const createElement = createElementHandler(landingPage, setLandingPage);
+	const deleteHandler = deleteElementHandler(landingPage, setLandingPage);
 
 	return (
 		<div className="app-container">
 			<div className="editor-container">
-				<JsonEditor
-					onRender={handleRender}
-					errorMessage={errorMessage}
-					value={landingData}
-					onChange={setLandingData}
-				/>
 				<EditArea>
 					<ActionMenu>
 						<ActionListMenuItem name="Create component">
