704e0ebe8c893c304fbe5f8014a9f28a43d704f4
Alexey Grishin <alexgrishin5556@gmail.com>
2026-06-07 15:53:24 +0300

    Support project ID definition in query parameters (#34)

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
 function App() {
+	const params = new URLSearchParams(window.location.search);
+	const projectId = params.get("projectId");
+
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<EditorPage />} />
+				<Route path="/edit" element={<EditorPage projectId={projectId} />} />
 				<Route path="/login" element={<LoginPage />} />
 			</Routes>
 		</BrowserRouter>
